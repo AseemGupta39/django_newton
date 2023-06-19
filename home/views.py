d9e1340c36cb0d62a9de76214b59f055c8e90897
Aseem Gupta <aseemguptajaipur@gmail.com>
2023-06-20 00:39:43 +0530

    4th video 19 min 15 sec tak kar liya

--- home/views.py
+++ home/views.py
@@ -5,9 +5,9 @@ def home(request):
     text =  """Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam sit perspiciatis ipsa, nihil 
     omnis iure doloremque, voluptates incidunt reiciendis asperiores, ipsum minima illum provident earum iste
     eius inventore. Sint, fugiat?"""
-     
+    veg=['carrot','pumpkin','ladyfinger']
     p=[ {'name':'re1','age':15} ,{'name':'re3','age':18} ,{'name':'re5','age':19} ,{'name':'re7','age':17} ]
-    return render(request,'index.html',context={'p':p,'text':text})
+    return render(request,'index.html',context={'p':p,'text':text,'veg':veg})
     # return HttpResponse("<h1>bhai tu sahi jagah aya hai</h1>")
     # return HttpResponse("""<h1 style = "text-align: center";>bhai tu sahi jagah aya hai</h1>""")
 
