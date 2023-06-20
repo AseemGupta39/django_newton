ac745490f35558ce6010586a969cde21d4c1223b
Aseem Gupta <aseemguptajaipur@gmail.com>
2023-06-21 00:27:34 +0530

    5th video 8min 55sec tak kar liya

--- home/models.py
+++ home/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+# from django.db import models
+from django.db.models import Model,CharField,IntegerField,EmailField,TextField,ImageField,FileField
 
 # Create your models here.
+
+class Student(Model):
+    name = CharField(max_length=100)
+    age = IntegerField()
+    email = EmailField()
+    address = TextField()
+    image = ImageField()
+    file = FileField()
+
+class Product(Model):
+    pass
+
+# s = []
+# start = end = maxlength = 0
+# l = []
+# n=len(s)
+# while(end<n):
+#     if s[end] not in l:
+#         l.append[s[end]]
+#         end+=1
+#         maxlength=max(maxlength,len(l))
+#     else:
+#         l.pop(l.index(s[start]))
+#         start+=1
+
+# print(maxlength)
--- home/views.py
+++ home/views.py
@@ -7,16 +7,19 @@ def home(request):
     eius inventore. Sint, fugiat?"""
     veg=['carrot','pumpkin','ladyfinger']
     p=[ {'name':'re1','age':15} ,{'name':'re3','age':18} ,{'name':'re5','age':19} ,{'name':'re7','age':17} ]
-    return render(request,'index.html',context={'p':p,'text':text,'veg':veg})
+    return render(request,'index.html',context={'page':'django tutorial','p':p,'text':text,'veg':veg})
     # return HttpResponse("<h1>bhai tu sahi jagah aya hai</h1>")
     # return HttpResponse("""<h1 style = "text-align: center";>bhai tu sahi jagah aya hai</h1>""")
 
 def about(request):
-    return render(request,'about.html')
+    context = {'page':'about'}
+    return render(request,'about.html',context=context)
 
 def contact(request):
-    return render(request,'contact.html')
+    context = {'page':'contact'}
+    return render(request,'contact.html',context=context)
 
 def success_page(request):
+    context = {'page':'contact'}
     # return render(request,'index.html')
-    return HttpResponse("<h1>you landed on my land</h1>")
+    return HttpResponse("<h1>you landed on my land</h1>",context=context)
