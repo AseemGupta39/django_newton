008990f6e252d67a6dc28e641d77db0772fd4725
Aseem <aseemguptajaipur@gmail.com>
2023-07-08 22:44:19 +0530

    18th video completd fully

--- vege/views.py
+++ vege/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 def register(request):
      if request.method == "POST":
@@ -126,8 +127,16 @@ def receipes(request):
 
 def get_students(request):
      queryset = Student.objects.all()
-     paginator = Paginator(queryset, 25)  # Show 25 contacts per page.
 
+     if request.GET.get('search'):
+          search = request.GET.get('search')
+          queryset = queryset.filter(Q(department__department__icontains=search) |
+                                   Q(student_name__icontains=search) | 
+                                   Q(student_email__icontains=search) |
+                                   Q(student_id__student_id__icontains=search) |
+                                   Q(student_age__icontains=search) )
+     # | Q(student_id__icontains = search)
+     paginator = Paginator(queryset, 25)  # Show 25 contacts per page.
      page_number = request.GET.get("page",1)
      page_obj = paginator.get_page(page_number)
      print(page_obj.object_list)
